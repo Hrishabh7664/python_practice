9c46b3baca0c8c173d434e1fde3d5e7c6395cfe1
Hrishabh7664 <hrishabh029@gmail.com>
2026-07-15 22:29:30 +0530

    farenheit to celsius

---
+++ lup4.py
@@ -0,0 +1,6 @@
+#assume a suitable value for temperature of a city in farenhit 
+#write program to convert this temperature in celsius and print both temp 
+f = 70 
+c = (f - 32) * 5/9
+print("farenhit temp:",f)
+print("celsius temp:",c)
