0b5457eb907bc6491e5557c11aba2518de0f310f
Hrishabh7664 <hrishabh029@gmail.com>
2026-07-24 21:56:59 +0530

    java speed problem

---
+++ javap/ITJ7.java
@@ -0,0 +1,19 @@
+public class ITJ7{
+    public static void main(String[] args) {
+        float time , distanceKM , distanceMILE , speed;
+        time = 100.35f/60;
+        distanceMILE = 24f;
+        distanceKM = distanceMILE*1.6f;
+
+
+
+
+
+
+        speed = distanceKM/time;
+        
+    System.out.println(speed);
+    
+   
+    }
+}
